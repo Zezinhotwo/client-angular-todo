a0a7dbd02ddc16adf76af947eb2db7d3b03654d9
ZezinhoTwo <107373132+Zezinhotwo@users.noreply.github.com>
2026-04-08 01:40:20 -0300

    fix bug render todo-html.component

--- src/app/components/todo/todo.component/todo.component.ts
+++ src/app/components/todo/todo.component/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { TodoApiServices } from '../../../services/todo-api.service';
 import { Todo } from '../../../models/todo';
 import { TodoFormComponent } from '../todo-form/todo-form.component';
@@ -14,7 +14,10 @@ export class TodoComponent implements OnInit {
   todos: Todo[] = [];
   todo?: Todo;
   showForm: boolean = false;
-  constructor(private service: TodoApiServices) {}
+  constructor(
+    private service: TodoApiServices,
+    private cd: ChangeDetectorRef,
+  ) {}
 
   ngOnInit(): void {
     this.loadTodos();
@@ -23,12 +26,14 @@ export class TodoComponent implements OnInit {
   loadTodos() {
     this.service.getTodos().subscribe((data) => {
       this.todos = data;
+      this.cd.detectChanges();
     });
   }
 
   createTodo(todo: Todo) {
-    this.service.postTodo(todo).subscribe(() => {
-      this.loadTodos();
+    this.service.postTodo(todo).subscribe((newTodo) => {
+      this.todos.push(newTodo);
+       this.cd.detectChanges();
     });
   }
 
